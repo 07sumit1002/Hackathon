92e7432bd7fca5d21e2906decffd49f221ea8480
07sumit1002 <sumitkumar101984@gmail.com>
2023-08-03 13:08:03 +0530

    Update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -37,10 +37,10 @@ export default function Home() {
                   <img className='Home-logoimg' src={Logo} alt='' />
               </div>
               <ul className="nav-links">
-              <li><a href="/">Home</a></li>
-              <li><a href="/contact">Contact</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/about">About Us</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+              <li><Link to="/services">Services</Link></li>
+              <li><Link to="/about">About Us</Link></li>
               </ul>
           </div>
           <div className="Home-tt">
